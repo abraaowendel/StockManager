78e9341fa9912d09afd5115501045a96c842acbf
Abraao Wendel <abraaowendel@gmail.com>
2023-05-18 18:18:14 -0300

    [Exceções Personalizadas]: ResourceNotFoundException e DataBaseException foram implementadas.

---
+++ src/main/java/com/aw/stockmanager/services/exceptions/DataBaseException.java
@@ -0,0 +1,7 @@
+package com.aw.stockmanager.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
