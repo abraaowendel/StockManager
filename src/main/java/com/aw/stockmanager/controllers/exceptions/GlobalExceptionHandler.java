ea3a06768b3cd0e35b29c86b59f65a2413895dfe
Abraao Wendel <abraaowendel@gmail.com>
2023-05-18 18:20:07 -0300

    [Lidar com erros]: O GlobalExceptionHandler foi implementado, ele captura os erros vindo de requisições feitas no controlador.

--- src/main/java/com/aw/stockmanager/controllers/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/aw/stockmanager/controllers/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,27 @@
 package com.aw.stockmanager.controllers.exceptions;
 
+import com.aw.stockmanager.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErroDetails> handleResourceNotFoundException(ResourceNotFoundException exception){
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        return ResponseEntity.status(httpStatus).body(copiarDados(httpStatus, exception.getMessage()));
+    }
+
+    private ErroDetails copiarDados(HttpStatus status, String message) {
+        ErroDetails error = new ErroDetails();
+        error.setCode(status.value());
+        error.setMessage(message);
+        error.setTimestamp(Instant.now());
+        return error;
+    }
 
 }
