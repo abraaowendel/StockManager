61d0df7e85c8564caf81a88861528cdc752058ad
Abraao Wendel <abraaowendel@gmail.com>
2023-05-19 15:08:21 -0300

    [Entidade Categoria]: Entidade categoria e CategoriaDTO foram criados e implementadas as devidas validações.

---
+++ src/main/java/com/aw/stockmanager/model/dto/CategoriaDTO.java
@@ -0,0 +1,39 @@
+package com.aw.stockmanager.model.dto;
+
+import com.aw.stockmanager.model.entities.Categoria;
+
+import java.io.Serializable;
+
+public class CategoriaDTO implements Serializable {
+    private static final long serialVersionUUID = 1L;
+    private Long id;
+    private String nome;
+
+    public CategoriaDTO() {
+    }
+
+    public CategoriaDTO(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+    public CategoriaDTO(Categoria entity) {
+        id = entity.getId();
+        nome = entity.getNome();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
