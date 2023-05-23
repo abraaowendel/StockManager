1db21e3bf17fc521d46ff6be1f9b56c389a05546
Abraao Wendel <abraaowendel@gmail.com>
2023-05-23 11:19:37 -0300

    Entidade Fornecedor foi refatorada. Service, Repository e DTO foram implementados para uso.

--- src/main/java/com/aw/stockmanager/repositories/FornecedorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aw.stockmanager.repositories;
-
-import com.aw.stockmanager.model.entities.Fornecedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
-}
