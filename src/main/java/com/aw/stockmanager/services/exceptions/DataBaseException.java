6bcddb172a8bffbf02772fd9a701ebb2a6631452
Abraao Wendel <abraaowendel@gmail.com>
2023-05-23 11:20:52 -0300

    Nova exception personalizada adicionada.

--- src/main/java/com/aw/stockmanager/services/exceptions/DataBaseException.java
+++
@@ -1,7 +0,0 @@
-package com.aw.stockmanager.services.exceptions;
-
-public class DataBaseException extends RuntimeException {
-    public DataBaseException(String message) {
-        super(message);
-    }
-}
